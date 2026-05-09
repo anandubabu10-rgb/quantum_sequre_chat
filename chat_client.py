63f3d15fe3444f9a8f2047feab574c0f80599d0f
Anjali Krishnan <anandubabu10@gmail.com>
2026-05-09 20:09:18 +0530

    Added gunicorn

--- chat_client.py
+++ chat_client.py
@@ -15,7 +15,8 @@ from encryption import (
 )
 
 # CHANGE THIS AFTER DEPLOYMENT
-SERVER_URL = "http://127.0.0.1:5000"
+# SERVER_URL = "http://127.0.0.1:5000"
+SERVER_URL = https://quantum-sequre-chat.onrender.com"
 
 username = input(
     "Enter your username: "
